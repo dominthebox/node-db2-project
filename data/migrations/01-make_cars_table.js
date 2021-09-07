fd0620ded895f8801b0abd7a9d941e080bf4dbd2
dominthebox <dsallustro@gmail.com>
2021-09-07 10:30:29 -0400

    first commit, testing codegrade wiring, wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,21 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments()
+
+    tbl.string('vin', 13).notNullable().unique()
+
+    tbl.string('make', 100).notNullable()
+
+    tbl.string('model', 100).notNullable()
+
+    tbl.integer('mileage').unsigned().notNullable()
+
+    tbl.string('title', 100).defaultTo('clean')
+
+    tbl.string('transmission', 100)
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1234567891234',
+        make: 'subaru',
+        model: 'WRX',
+        mileage: 59876,
+        title: 'clean',
+        transmission: 'manuel',
+    },
+    {
+        vin: '2345678912345',
+        make: 'toyota',
+        model: 'corolla',
+        mileage: 69890,
+        title: 'dirty',
+        transmission: 'automatic',
+    },
+    {
+        vin: '3456789123456',
+        make: 'lamborghini',
+        model: 'diablo',
+        mileage: 22654,
+        title: 'clean',
+        transmission: 'manuel',
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
