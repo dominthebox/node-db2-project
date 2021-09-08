6828e30a39c2bb49a755dce403e3418517ce3458
dominthebox <dsallustro@gmail.com>
2021-09-08 00:53:25 -0400

    all tests now passing

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -8,12 +8,20 @@ const getById = (id) => {
   return db('cars').where('id', id).first()
 }
 
-const create = () => {
-  return 
+const getByVin = (vin) => {
+  return db('cars').where('vin', vin).first()
+}
+
+const create = (car) => {
+  return db('cars').insert(car)
+    .then(([id]) => {
+      return getById(id)
+    })
 }
 
 module.exports = {
   getAll,
   getById,
+  getByVin,
   create
 }
