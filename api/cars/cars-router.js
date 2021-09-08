9b5928d4f845c6e31c0c496b52214e513f023ac1
dominthebox <dsallustro@gmail.com>
2021-09-07 23:47:44 -0400

    wip, wired up the cars router endpoints

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,18 @@
 // DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.json('getting all cars')
+})
+
+router.get('/:id', (req, res, next) => {
+    res.json(`getting car with id ${req.params.id}`)
+})
+
+router.post('/', (req, res, next) => {
+    res.json('posting new car')
+})
+
+module.exports = router
